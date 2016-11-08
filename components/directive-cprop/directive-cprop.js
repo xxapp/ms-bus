233a07e2995fa35d4ac2e6b291b8901cce9f3cc6
mengxiangxiang <mengxiangxiang@cdavatar.com>
2016-11-08 15:21:36 +0800

    组件：去掉解决问题时多余的逻辑

--- components/directive-cprop/directive-cprop.js
+++ components/directive-cprop/directive-cprop.js
@@ -3,7 +3,7 @@ var avxUtil = require('/vendor/avx-component/avx-util');
 
 avalon.directive('cprop', {
     init: function (binding) {
-        var elem = binding.element, parent = elem.parentNode, signature, comment;
+        var elem = binding.element, parent = elem.parentNode;
         var vmodels = binding.vmodels;
         var vmChain = [];
         for (var i in vmodels) {
@@ -14,11 +14,6 @@ avalon.directive('cprop', {
         elem.setAttribute('vm-chain', vmChain.join(','));
         elem.setAttribute('prop-' + binding.param, binding.expr);
 
-        if (elem.previousSibling && (elem.previousSibling.nodeType != 8 || elem.previousSibling.nodeValue.indexOf('cprop') != 0)) {
-            signature = avxUtil.generateID('cprop');
-            binding.mark = comment = document.createComment(signature)
-            parent.insertBefore(comment, elem);
-        }
         binding.rollback = function () {
         }
     },
