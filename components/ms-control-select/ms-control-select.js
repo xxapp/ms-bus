6b4a2cf53cbc785d23af3771ad4b083e1945593b
xxapp <whomakepie@outlook.com>
2017-02-16 13:57:57 +0800

    组件：用ms-cduplex改造control-select组件

--- components/ms-control-select/ms-control-select.js
+++ components/ms-control-select/ms-control-select.js
@@ -5,8 +5,7 @@ var avxUtil = require('/vendor/avx-component/avx-util');
 /**
  * 选择组件
  * @prop label 选择框前的label标签内容
- * @prop col 如果有绑定的数据行，此属性值指的是数据的字段名称
- * @prop duplex 自定义的绑定数据，如果同时存在则会覆盖col
+ * @prop col 指定name属性值
  * @prop store 数据源，组件会自动寻找数据源中的dict方法，如果dict没定义则使用list方法
  * @prop col-key 数据中作为select的value属性值的字段，默认为id
  * @prop col-val 数据中作为select的展示值的字段，默认为name
@@ -25,26 +24,19 @@ avalon.component('ms:controlSelect', {
     content: '',
     $template: __inline('./ms-control-select.html'),
     $replace: 1,
+    $dynamicProp: {
+        duplex: { type: 'String' }
+    },
     $$template: function (tmpl) {
         var vm = this;
-        var $parent = avalon.vmodels[this.parentVmId];
-        this.model = this.model || ($parent && $parent.model) || 'record';
         if (vm.store) {
             tmpl = tmpl.replace(/\{\{content\|html\}\}/g, '<option ms-repeat="options" ms-attr-value="el.' + vm.colKey + '">{{el.' + vm.colVal + '}}</option>');
-        }
-        
-        if (this.duplex) {
-            // 如果配置了duplex属性，则直接使用duplex的属性值绑定控件
-            tmpl =  tmpl.replace(/ms-duplex="record\[col\]"/g, 'ms-duplex="' + this.duplex + '"');
-        }
-        if (this.col) {
-            // 否则用col的配置，使用record[col]去绑定控件
-            tmpl =  tmpl.replace(/ms-duplex="record\[col\]"/g, 'ms-duplex="' + this.model + '[\'' + this.col.replace('.', '\'][\'') + '\']"');
         }
         return tmpl;
     },
     $init: function (vm, el) {
         vm.$parentVmId = avxUtil.pickToRefs(vm, el);
+        avxUtil.enableDynamicProp(vm, el);
         if (vm.store && !store[vm.store]) { avalon.error('配置了数据源，但数据源[' + vm.store + ']似乎未定义，/services/storeService.js') }
         
         if (vm.store) {
@@ -55,6 +47,10 @@ avalon.component('ms:controlSelect', {
                 vm.options = result.list;
             });
         }
+
+        vm.$watch('duplex', function (v) {
+            vm.$dynamicProp.duplex.setter(v);
+        });
     },
     $dispose: function (vm, el) {
         avxUtil.removeFromRefs(vm, el);
@@ -66,6 +62,5 @@ avalon.component('ms:controlSelect', {
     store: '',
     colKey: 'id',
     colVal: 'name',
-    options: [],
-    model: ''
+    options: []
 });
