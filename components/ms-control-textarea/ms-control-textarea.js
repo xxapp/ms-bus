a06926644d48a0c5f2f15b3ffe331304dceaef4b
xxapp <whomakepie@outlook.com>
2017-02-16 13:48:00 +0800

    组件：用ms-cduplex改造control-textarea组件

--- components/ms-control-textarea/ms-control-textarea.js
+++ components/ms-control-textarea/ms-control-textarea.js
@@ -4,36 +4,28 @@ var avxUtil = require('/vendor/avx-component/avx-util');
 /**
  * 多行文本输入组件
  * @prop label 文本框前的label标签内容
- * @prop col 如果有绑定的数据行，此属性值指的是数据的字段名称
- * @prop duplex 自定义的绑定数据，如果同时存在则会覆盖col
+ * @prop col 指定name属性值
  * @prop rows 文本框行数
  * 
  * @example
  * ``` html
  * <!-- 注：例1和例2效果是一样的 -->
- * <ms:control-textarea label="标题1" col="name"></ms:control-textarea>
- * <ms:control-textarea label="标题2" duplex="record['name']"></ms:control-textarea>
- * <ms:control-textarea label="标题3" duplex="state.text"></ms:control-textarea>
+ * <ms:control-textarea label="标题1" col="name" ms-cduplex="record.name"></ms:control-textarea>
  * ```
  */
 avalon.component('ms:controlTextarea', {
     $template: __inline('./ms-control-textarea.html'),
     $replace: 1,
-    $$template: function (tmpl) {
-        var $parent = avalon.vmodels[this.parentVmId];
-        this.model = this.model || ($parent && $parent.model) || 'record';
-        if (this.duplex) {
-            // 如果配置了duplex属性，则直接使用duplex的属性值绑定控件
-            return tmpl.replace(/ms-duplex="record\[col\]"/g, 'ms-duplex="' + this.duplex + '"');
-        }
-        if (this.col) {
-            // 否则用col的配置，使用record[col]去绑定控件
-            return tmpl.replace(/ms-duplex="record\[col\]"/g, 'ms-duplex="' + this.model + '[\'' + this.col.replace('.', '\'][\'') + '\']"');
-        }
-        return tmpl;
+    $dynamicProp: {
+        duplex: { type: 'String' }
     },
     $init: function (vm, el) {
         vm.$parentVmId = avxUtil.pickToRefs(vm, el);
+        avxUtil.enableDynamicProp(vm, el);
+
+        vm.$watch('duplex', function (v) {
+            vm.$dynamicProp.duplex.setter(v);
+        });
     },
     $dispose: function (vm, el) {
         avxUtil.removeFromRefs(vm, el);
@@ -42,6 +34,5 @@ avalon.component('ms:controlTextarea', {
     label: '',
     col: '',
     duplex: '',
-    rows: '',
-    model: ''
+    rows: ''
 });
