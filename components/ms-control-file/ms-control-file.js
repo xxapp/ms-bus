9fce2489baac2a2e0bb8d264d6ecb63c981a9108
xxapp <whomakepie@outlook.com>
2017-02-16 14:06:56 +0800

    组件：用ms-cduplex改造control-file组件

--- components/ms-control-file/ms-control-file.js
+++ components/ms-control-file/ms-control-file.js
@@ -5,39 +5,34 @@ var avxUtil = require('/vendor/avx-component/avx-util');
 /**
  * 文件输入组件
  * @prop label 文本框前的label标签内容
- * @prop col 如果有绑定的数据行，此属性值指的是数据的字段名称
- * @prop duplex 自定义的绑定数据，如果同时存在则会覆盖col
+ * @prop col 指定name属性值
+ * @prop store 处理文件上传的store配置，默认为"file"
+ * @prop placeholder 占位文字
+ * @prop btnText 上传按钮文字，默认为"选择文件"
  * 
  * @example
  * ``` html
  * <!-- 注：例1和例2效果是一样的 -->
- * <ms:control-file label="标题1" col="name"></ms:control-file>
- * <ms:control-file label="标题2" duplex="record['name']"></ms:control-file>
- * <ms:control-file label="标题3" duplex="state.text"></ms:control-file>
+ * <ms:control-file label="标题1" col="name" ms-cduplex="record.name"></ms:control-file>
  * ```
  */
 avalon.component('ms:controlFile', {
     $template: __inline('./ms-control-file.html'),
     $replace: 1,
-    $$template: function (tmpl) {
-        var $parent = avalon.vmodels[this.parentVmId];
-        this.model = this.model || ($parent && $parent.model) || 'record';
-        if (this.duplex) {
-            // 如果配置了duplex属性，则直接使用duplex的属性值绑定控件
-            return tmpl.replace(/ms-duplex="record\[col\]"/g, 'ms-duplex="' + this.duplex + '"');
-        }
-        if (this.col) {
-            // 否则用col的配置，使用record[col]去绑定控件
-            return tmpl.replace(/ms-duplex="record\[col\]"/g, 'ms-duplex="' + this.model + '[\'' + this.col.replace('.', '\'][\'') + '\']"');
-        }
-        return tmpl;
+    $dynamicProp: {
+        duplex: { type: 'String' }
     },
     $init: function (vm, el) {
         vm.$parentVmId = avxUtil.pickToRefs(vm, el);
+        avxUtil.enableDynamicProp(vm, el);
         if (!store[vm.store]) {
             avalon.error('数据源[' + vm.store + ']似乎未定义，检查/services/storeService.js');
         }
         vm.helpId = vm.$id;
+
+        vm.$watch('duplex', function (v) {
+            vm.$dynamicProp.duplex.setter(v);
+        });
     },
     $ready: function (vm, el) {
         function changeHandler() {
@@ -65,6 +60,5 @@ avalon.component('ms:controlFile', {
     duplex: '',
     placeholder: '',
     btnText: '选择文件',
-    helpId: '',
-    model: ''
+    helpId: ''
 });
