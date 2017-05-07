f9a20a799059f440a38aee837fab9567687c2f94
xxapp <552062803@qq.com>
2017-05-07 16:11:20 +0800

    完成upload组件的基本功能

--- components/ms-upload/ms-upload.ts
+++ components/ms-upload/ms-upload.ts
@@ -26,22 +26,16 @@ controlComponent.extend({
         trigger: '',
         value: [],
         fileList: [],
+        action: '',
         listType: 'text-list',
         $uploader: null,
         handleRemove(file) {
-            // this.fileList.forEach((f, i) => {
-            //     if (f.uid === file.uid) {
-            //         this.fileList.removeAt(i);
-            //         return false;
-            //     }
-            // });
-            // this.fileList.push({
-            //     uid: -2,
-            //     name: 'doge.jpg',
-            //     url: 'http://www.baidu.com',
-            //     status: 'done'
-            // });
             this.fileList.removeAll(f => f.uid === file.uid);
+            this.value.remove(file.url);
+            this.handleChange({
+                target: { value: this.value.$model || this.value },
+                type: 'file-upload'
+            });
         },
         onInit(event) {
             emitToFormItem(this);
@@ -51,65 +45,66 @@ controlComponent.extend({
                     uid: -(i + 1),
                     name: url.replace(/.*\/([^\/]+)\/?/, '$1'),
                     url: url,
-                    status: 'done'
+                    status: 'done',
+                    progress: 0
                 });
             });
         },
         onReady(event) {
             this.$uploader = Uploader.init({
-                url: '/api/file/uploadFile',
+                url: this.action,
                 fileInput: event.target.getElementsByTagName('input').file,
-                onSelect: (files) => {
-                    console.log('selected: ', files);
-                    files.map(file => {
-                        if (this.fileList.every(f => f.index !== file.index)) {
+                onSelect: (files, allFiles) => {
+                    allFiles.map(file => {
+                        if (this.fileList.every(f => f.uid !== file.index)) {
                             this.fileList.push({
                                 uid: file.index,
-                                name: file.name
+                                name: file.name,
+                                status: 'uploading',
+                                progress: 0
+                            });
+                        } else {
+                            updateFileObj(this.fileList, file.index, f => {
+                                f.status = 'uploading';
+                                f.progress = 0;
                             });
                         }
                     });
                     this.$uploader.upload();
                 },
-                onFinish: (file) => {
-                    console.log('deleted: ', file.index);
-                },
                 onProgress: (file, loaded, total) => {
-                    console.log('progressed: ', file, loaded, total);
+                    updateFileObj(this.fileList, file.index, f => f.progress = (loaded / total * 100).toFixed());
                 },
                 onSuccess: (file, response) => {
-                    console.log('successed: ', file, response);
+                    updateFileObj(this.fileList, file.index, f => {
+                        f.status = 'done';
+                        f.progress = 100;
+                        f.url = response.url;
+                    });
                     this.value.push(response.url);
                 },
                 onFailure: (file, err) => {
-                    console.log('faileured: ', file, err);
+                    updateFileObj(this.fileList, file.index, f => f.status = 'error')
                     throw err;
                 },
                 onComplete: () => {
-                    console.log('completed');
-                    this.onChange({
+                    this.handleChange({
                         target: { value: this.value.$model || this.value },
                         type: 'file-upload'
                     });
                 }
             });
-            // function changeHandler() {
-            //     var inputId = 'file' + vm.$id, val = this.value, index = val.lastIndexOf('\\');
-            //     this.id = inputId;
-            //     $(el).find('input:text').val('文件' + val.substring(index + 1) + '上传中...');
-            //     file.insert({
-            //         fileElementId: inputId,
-            //         success: function (data, status) {
-            //             vm.duplex = data.url;
-            //             $(el).find('input:text').trigger('input');
-            //         }
-            //     });
-            //     $(this).replaceWith('<input type="file" name="file">');
-            //     $(el).find('input:file').change(changeHandler);
-            // }
-            // $(el).find('input:file').change(changeHandler);
         },
         onDispose(event) {
         }
     }
 });
+
+function updateFileObj(fileList, uid, callback) {
+    fileList.forEach(f => {
+        if (f.uid === uid) {
+            callback(f);
+            return false;
+        }
+    });
+}
--- mock/file/insert.js
+++ mock/file/insert.js
@@ -1,6 +1,9 @@
 module.exports = function(req, res, next) {
     res.set('Content-Type', 'text/html');
-    res.json({
-        "url": "http://www.androidpolice.com/wp-content/uploads/2014/07/nexusae0_OnePlus_logo_thumb.png"
-    });
+    // res.setStatus('404');
+    setTimeout(function () {
+        res.json({
+            "url": "http://www.androidpolice.com/wp-content/uploads/2014/07/nexusae0_OnePlus_logo_thumb.png"
+        });    
+    }, 2000);
 };
