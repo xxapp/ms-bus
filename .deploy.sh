86a69ec3e87a5501cc885fbe8f7f8b6ba9d78c84
xxapp <whomakepie@outlook.com>
2017-05-12 16:07:09 +0800

    添加 travis ci 部署脚本

---
+++ .deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 使用fis进行构建
+fis3 release -d ./output
+
+if [ -d output ]; then
+    # 提交代码至gh-pages分支
+    echo "➥ Commit files"
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
+    rm -rf ${REPO_SLUG}/*
+    cp -rf output/* ${REPO_SLUG}
+    cd ${REPO_SLUG}
+    git status
+    git config user.email "travis@travis-ci.org"
+    git config user.name "travis-ci"
+    git add -A
+    git commit -m "[ci skip] publish gh-pages"
+    git push -fq origin gh-pages > /dev/null
+else
+    echo '➥ Fail'
+    exit 1
+fi
