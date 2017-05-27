2240495928f98fd20a0a1b2002ea682817337c3f
xxapp <whomakepie@outlook.com>
2017-05-27 17:05:38 +0800

    [ci skip]测试 layout 去掉有问题的demo

---
+++ index-new.ts
@@ -0,0 +1,27 @@
+import 'es5-shim';
+if (getInternetExplorerVersion() === 8) {
+    Object.defineProperty = function (obj, property, meta) {
+        obj[property] = meta.value;
+    }
+}
+import 'es5-shim/es5-sham';
+import 'es6-promise/dist/es6-promise.auto';
+import { message } from 'ane';
+// import * as jQuery from 'jquery';
+// global.$ = global.jQuery = jQuery;
+
+message.success({
+    content: 'ok'
+})
+
+function getInternetExplorerVersion() {
+    let rv = -1;
+    if (navigator.appName === 'Microsoft Internet Explorer') {
+        const ua = navigator.userAgent;
+        const re = new RegExp('MSIE ([0-9]{1,}[\.0-9]{0,})');
+        if (re.exec(ua) !== null) {
+            rv = parseFloat(RegExp.$1);
+        }
+    }
+    return rv;
+}
