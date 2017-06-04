d731175b246269a00cbca3ffa49392393d2af198
xxapp <552062803@qq.com>
2017-06-05 00:40:35 +0800

    defineProperty 的 polyfill 放到 avalon 的后面，解决兼容问题

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 import 'es5-shim';
-if (getInternetExplorerVersion() === 8) {
-    Object.defineProperty = function (obj, property, meta) {
-        obj[property] = meta.value;
-    }
-}
-import 'es5-shim/es5-sham';
 import 'es6-promise/dist/es6-promise.auto';
 import * as jQuery from 'jquery';
 global.$ = global.jQuery = jQuery;
 import 'bootstrap';
 import * as avalon from 'avalon2';
+if (avalon.msie === 8) {
+    Object.defineProperty = function (obj, property, meta) {
+        obj[property] = meta.value;
+    }
+}
+import 'es5-shim/es5-sham';
 import './services/routerService';
 import { breadcrumb } from './services/storeService';
 import 'ane/components/ms-layout';
@@ -17,7 +17,8 @@ import 'ane/components/ms-layout';
 const root = avalon.define({
     $id: 'root',
     currentPage: '',
-    breadcrumb: []
+    breadcrumb: [],
+    user: {}
 });
 breadcrumb.list$.subscribe(v => {
     root.breadcrumb = v;
@@ -29,15 +30,3 @@ if (!/#!/.test(global.location.hash)) {
     avalon.router.navigate('/', 2);
 }
 avalon.scan(document.body);
-
-function getInternetExplorerVersion() {
-    let rv = -1;
-    if (navigator.appName === 'Microsoft Internet Explorer') {
-        const ua = navigator.userAgent;
-        const re = new RegExp('MSIE ([0-9]{1,}[\.0-9]{0,})');
-        if (re.exec(ua) !== null) {
-            rv = parseFloat(RegExp.$1);
-        }
-    }
-    return rv;
-}
